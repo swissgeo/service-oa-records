23b223e9c4d5061737d73f1f032f996c8cc42d7e
Khôi Tran <kt@compile.ch>
2026-04-01 09:12:06 +0200

    GPS-598: properly remove server url

--- pygeoapi-swissgeo-extensions/app.py
+++ pygeoapi-swissgeo-extensions/app.py
@@ -20,13 +20,15 @@ _original_call_api_threadsafe = _starlette_mod.call_api_threadsafe
 def _call_api_threadsafe_with_lang(loop, api_function, actual_api, api_request, *args):
     headers = api_request.headers
     host = headers.get("host", "")
-    scheme = headers.get("x-forwarded-proto", "https")
+    scheme = headers.get("x-forwarded-proto", "http")
     set_request_params(
         lang=api_request._args.get("lang", None),
         fmt=api_request._args.get("f", None),
         server_url=f"{scheme}://{host}" if host else None,
     )
-    return _original_call_api_threadsafe(loop, api_function, actual_api, api_request, *args)
+    return _original_call_api_threadsafe(
+        loop, api_function, actual_api, api_request, *args
+    )
 
 
 _starlette_mod.call_api_threadsafe = _call_api_threadsafe_with_lang
